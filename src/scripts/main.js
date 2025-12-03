a59c97eabd8e1a3f1944ac60673b6a68d7482cfd
A-Melnykk <969696245n@gmail.com>
2025-12-03 20:23:10 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+const gallery = document.getElementById('thumbs');
+
+gallery.addEventListener('click', function (e) {
+  event.preventDefault();
+
+  let target = event.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.parentNode;
+  }
+
+  if (target.tagName !== 'A') {
+    return;
+  }
+
+  const newSrc = target.href;
+
+  mainImage.src = newSrc;
+});
